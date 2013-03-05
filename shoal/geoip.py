ab479a7c507af8d9d33cc990e42ca258884e1b1e
Mike Chester <mchester@uvic.ca>
2013-03-05 14:38:00 -0800

    Implemented more pythonic version of haversine eqn

--- shoal/geoip.py
+++ shoal/geoip.py
@@ -1,11 +1,12 @@
 import sys
 import os
 import subprocess
-import pygeoip, math
+import pygeoip
 import web
 import logging
 import operator
 from time import time, sleep
+from math import radians, cos, sin, asin, sqrt
 
 import config
 
@@ -39,7 +40,7 @@ def get_nearest_squid(ip):
         s_lat = float(squid.geo_data['latitude'])
         s_long = float(squid.geo_data['longitude'])
 
-        distance = get_distance_between_nodes(r_lat,r_long,s_lat,s_long)
+        distance = haversine(r_lat,r_long,s_lat,s_long)
 
         if distance == smallest_distance:
             nearest_squid.append(squid)
@@ -60,24 +61,17 @@ def get_nearest_squid(ip):
 """
     Calculate distance between two points using Haversine Formula.
 """
-def get_distance_between_nodes(lat1,long1,lat2,long2):
+def haversine(lat1,lon1,lat2,lon2):
     # radius of earth
     r = 6371
-    d_lat = deg_to_rad(lat2-lat1)
-    d_long = deg_to_rad(long2-long1)
 
-    a = math.sin(d_lat/2) * math.sin(d_lat/2) + \
-        math.cos(deg_to_rad(lat1)) * math.cos(deg_to_rad(lat2)) * \
-        math.sin(d_long/2) * math.sin(d_long/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    d = r * c
-    return d
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
 
-"""
-    Helper functions
-"""
-def deg_to_rad(deg):
-    return deg * (math.pi/180)
+    return r * c
 
 def check_geolitecity_need_update():
     curr = time()
