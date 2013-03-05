4ee31c4f50c76d8b6e48f1f42f8e9c4376a1dd00
Mike Chester <mchester@uvic.ca>
2013-03-05 14:13:08 -0800

    client code to get nearest squid.

---
+++ get_nearest.py
@@ -0,0 +1,19 @@
+import urllib2
+import sys
+import json
+import logging
+
+from urllib2 import urlopen
+
+url = 'http://elephant68.heprc.uvic.ca:8080/nearest'
+
+try:
+    f = urlopen(url)
+except urllib2.URLError as e:
+    logging.error("Unable to open url. %s" % e)
+    sys.exit(1)
+
+data = json.loads(f.read())
+
+print 'Public IP:', data['public_ip']
+print 'Private IP:', data['private_ip']
