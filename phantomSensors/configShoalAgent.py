3b682c76f3eaf6339a5641a8681fc7d6eadfbe58
robprior <rjcprior@gmail.com>
2013-10-09 19:05:04 -0400

    Properly updates config file now, though it strips comments (this might not be trivial to fix based on quick googling)

--- phantomSensors/configShoalAgent.py
+++ phantomSensors/configShoalAgent.py
@@ -6,21 +6,22 @@ import ConfigParser
 import subprocess
 
 try:
+  homedir = os.path.expanduser('~')
+  configFile = os.path.abspath(homedir + "/shoal/shoal-agent/shoal_agent.conf")
   config = ConfigParser.ConfigParser()
-  config.read(os.path.abspath("~/shoal/shoal-agent/shoal_agent.conf"))
+  config.read(configFile)
   
   userDataServer  = subprocess.check_output(["cat", "/var/nimbus-metadata-server-url"])
   userDataJSONStr = subprocess.check_output(["curl", userDataServer + "/latest/user-data"])
-  print userDataJSONStr
   userDataJSON    = json.loads(userDataJSONStr)
 except Exception as e:
   print e
   sys.exit("Could not read config file or user data, exiting")
 
-#debugging
-print userDataJSON
+config.set('rabbitmq', "amqp_server_url", userDataJSON["rabbitMQServerIP"])
+config.set('rabbitmq', "amqp_port",       userDataJSON["rabbitMQServerPort"])
 
-config.set("rabbitMQ", "amqp_server_url", userDataJSON["rabbitMQServerIP"])
-config.set("rabbitMQ", "amqp_port",       userDataJSON["rabbitMQServerPort"])
+with open(configFile, "w") as cf:
+  config.write(cf)
 
 #good to start shoal agent
