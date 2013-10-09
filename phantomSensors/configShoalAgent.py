39511652b8ed8b806526e0d0555771119e3035df
root <root@vm-148-104.uc.futuregrid.org>
2013-10-09 18:17:59 -0400

    Adding test script populating shoal agent config with nimbus user data

---
+++ phantomSensors/configShoalAgent.py
@@ -0,0 +1,25 @@
+#!/bin/python
+import os
+import sys
+import json
+import ConfigParser
+import subprocess
+
+try:
+  config = ConfigParser.ConfigParser()
+  config.read(os.path.abspath("~/shoal/shoal-agent/shoal_agent.conf"))
+
+  return = subprocess.check_output(["cat", "/var/nimbus-metadata-server-url"])
+  userDataJSONStr = subprocess.chceck_output(["curl", return + "/latest/user-data"])
+  userDataJSON = json.loads(userDataJSONStr)
+except Exception as e:
+  print e
+  sys.exit("Could not read config file or user data, exiting")
+
+#debugging
+print userDataJSON
+
+config.set("rabbitMQ", "amqp_server_url", userDataJSON["rabbitMQServerIP])
+config.set("rabbitMQ", "amqp_port",       userDataJSON["rabbitMQServerPort])
+
+#good to start shoal agent
