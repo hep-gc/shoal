ec8db9b9e8f0c2bf2c8ce40530cf94647119c27a
root <root@vm-148-151.uc.futuregrid.org>
2013-10-11 12:34:04 -0400

    shoal agent creates two setup files, config now tries to update both

--- phantomSensors/configShoalAgent.py
+++ phantomSensors/configShoalAgent.py
@@ -1,27 +1,37 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 import os
 import sys
 import json
 import ConfigParser
 import subprocess
 
+
+def updateConfig(config, configFile, userDataJSON):
+  try:
+    config.read(configFile)  
+    config.set('rabbitmq', "amqp_server_url", userDataJSON["rabbitMQServerIP"])
+    config.set('rabbitmq', "amqp_port",       userDataJSON["rabbitMQServerPort"])
+    with open(configFile, "w") as cf:
+      config.write(cf)
+  except:
+    #this might not matter there are two config files
+    pass
+
 try:
-  homedir = os.path.expanduser('~')
-  configFile = os.path.abspath(homedir + "/shoal/shoal-agent/shoal_agent.conf")
-  config = ConfigParser.ConfigParser()
-  config.read(configFile)
-  
   userDataServer  = subprocess.check_output(["cat", "/var/nimbus-metadata-server-url"])
   userDataJSONStr = subprocess.check_output(["curl", userDataServer + "/latest/user-data"])
   userDataJSON    = json.loads(userDataJSONStr)
 except Exception as e:
   print e
-  sys.exit("Could not read config file or user data, exiting")
+  sys.exit("Could not read user data, exiting")
 
-config.set('rabbitmq', "amqp_server_url", userDataJSON["rabbitMQServerIP"])
-config.set('rabbitmq', "amqp_port",       userDataJSON["rabbitMQServerPort"])
+homedir = os.path.expanduser('~')
+#config file can be in two places
+configFile  = os.path.abspath(homedir + "/shoal/shoal-agent/shoal_agent.conf")
+configFile2 = "/etc/shoal/shoal_agent.conf"
+config = ConfigParser.ConfigParser()
 
-with open(configFile, "w") as cf:
-  config.write(cf)
+updateConfig(config, configFile,  userDataJSON)
+updateConfig(config, configFile2, userDataJSON)
 
-#good to start shoal agent
+#good to start shoal agen
