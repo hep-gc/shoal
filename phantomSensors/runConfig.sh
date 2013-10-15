81b80a6d043158075483d7845a64b24bfe605b00
robprior <rjcprior@gmail.com>
2013-10-15 18:46:55 -0400

    Script can't use relative if start up/login script. Also checks if shoal agent is already running before starting it again

--- phantomSensors/runConfig.sh
+++ phantomSensors/runConfig.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-TMP="`pwd`/configShoalAgent.py"
-eval $TMP
-service shoal_agent start
+TMP="~/shoal/phantomSensors/configShoalAgent.py"
+SHOAL_AGENT_STATUS="`service shoal_agent status`";
+if grep -q "shoal_agent is running" <<<$TESTVAR; then
+  exit 0
+else
+  eval $TMP
+  shoal-agent &
+fi
