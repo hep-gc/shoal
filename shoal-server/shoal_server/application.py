f118dd93a04d53d6134937159e7f462a8a082079
Mike Chester <mchester@uvic.ca>
2014-02-26 13:42:44 -0800

    added cleanse thread into tornado periodic callback

--- shoal-server/shoal_server/application.py
+++ shoal-server/shoal_server/application.py
@@ -22,15 +22,31 @@ class Application(tornado.web.Application):
         # setup rabbitmq connection
         self.rabbitmq = connections.setup_rabbitmq(self.global_settings, self.shoal)
 
+        # setup periodic squid cleanse (5 seconds).
+        tornado.ioloop.PeriodicCallback(self.cleanse,
+                self.global_settings["squid"]["cleanse_interval"]*1000,
+                io_loop=io_loop).start()
+
         tornado.web.Application.__init__(self, handlers, **config.settings['general'])
 
+    # Cleans up the inactive squids. Runs periodically.
+    def cleanse(self):
+        """updates and pops squid from shoal if it's inactive"""
+        curr = time.time()
+        print "cleaning your squeeds"
+        for squid in self.shoal.values():
+            if curr - squid.last_active > self.INACTIVE:
+                self.shoal.pop(squid.key)
 
 def run():
     io_loop = tornado.ioloop.IOLoop.instance()
 
     # pass io_loop so connections(pika) can hook into it.
     app = Application(io_loop)
+
+    # Hook RabbitMQ consumer into Tornado IOLoop
     io_loop.add_timeout(time.time() + .1, app.rabbitmq.run)
+
     app.listen(config.settings['general']['port'])
 
     io_loop.start()
