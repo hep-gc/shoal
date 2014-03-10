aad8efc869e0a47693ed42a2af77617921f11c8a
Mike Chester <mchester@uvic.ca>
2014-03-10 12:33:28 -0700

    convert templates to tornado style. Fixed a small bug in rabbitmq consumer which would overwrite the timestamp. Fixed handler to render the new templates

--- shoal-server/shoal_server/handlers.py
+++ shoal-server/shoal_server/handlers.py
@@ -1,9 +1,13 @@
 import tornado.web
+import time
 
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write(self.application.shoal)
+        shoal = self.application.shoal
+        sorted_shoal = [shoal[k] for k in sorted(shoal, key=shoal.get, reverse=True)]
+        inactive_time = self.application.global_settings['squid']['inactive_time']
+        self.render("index.html", shoal=sorted_shoal, inactive_time=inactive_time, now=time.time())
 
 class NearestHandler(tornado.web.RequestHandler):
     pass
