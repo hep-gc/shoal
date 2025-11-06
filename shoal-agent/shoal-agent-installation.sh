a9ca25c17bf02939428efce1a66af69c7d31ca02
Seojin Lee <seojinlee0070@gmail.com>
2025-11-06 12:58:27 -0800

    Remove cache configuration and detection logic
    Removed cache-related variables and functions from the installation script.

--- shoal-agent/shoal-agent-installation.sh
+++ shoal-agent/shoal-agent-installation.sh
@@ -27,9 +27,6 @@ DEFAULT_AMQP_EXCHANGE=''
 DEFAULT_LOG_FILE=''
 DEFAULT_LOGGING_LEVEL=''
 DEFAULT_ADMIN_EMAIL=root@localhost
-DEFAULT_CACHE_PORT=''
-DEFAULT_CACHE_TYPE=''
-DEFAULT_UPSTREAM=''
 
 OLD_INTERVAL=''
 OLD_AMQP_SERVER_URL=''
@@ -39,9 +36,6 @@ OLD_AMQP_EXCHANGE=''
 OLD_LOG_FILE=''
 OLD_LOGGING_LEVEL=''
 OLD_ADMIN_EMAIL=''
-OLD_CACHE_PORT=''
-OLD_CACHE_TYPE=''
-OLD_UPSTREAM=''
 
 ##################################################
 
@@ -79,31 +73,6 @@ setEachNewValue() {
         sed -i "s|$origin|$replace|g" $config_file
     fi
 
-}
-
-detectCacheType() {
-    if pidof squid >/dev/null 2>&1; then
-        echo "squid"
-    elif pidof varnishd >/dev/null 2>&1; then
-        echo "varnish"
-    else
-        echo ""
-    fi
-}
-
-getCachePort() {
-    local cache_type=$1
-    local default_port=""
-    
-    case "$cache_type" in
-        "squid")
-            default_port="3128"
-            ;;
-        "varnish")
-            default_port="6081"
-            ;;
-    esac 
-    echo "$default_port"
 }
 
 ###############################
@@ -192,12 +161,6 @@ if $USE_NOT_DEFAULT; then
             ;;
             "logging_level") DEFAULT_LOGGING_LEVEL=${line_array[1]}
             ;;
-			"squid_port") DEFAULT_CACHE_PORT=${line_array[1]}
-			;;
-		    "cache_type") DEFAULT_CACHE_TYPE=${line_array[1]}
-			;;
-			"upstream") DEFAULT_UPSTREAM=${line_array[1]}
-            ;;
         esac
     done <<< "$LINES"
 
@@ -224,12 +187,6 @@ if $USE_NOT_DEFAULT; then
                 ;;
                 "admin_email") OLD_ADMIN_EMAIL=${line_array[1]}
 				;;
-				"squid_port") OLD_CACHE_PORT=${line_array[1]}
-				;;
-				"cache_type") OLD_CACHE_TYPE=${line_array[1]}
-				;;
-				"upstream") OLD_UPSTREAM=${line_array[1]}
-                ;;
             esac
         done <<< "$OLD_LINES"
 
@@ -247,32 +204,7 @@ if $USE_NOT_DEFAULT; then
     setEachNewValue $CONFIG_FILE amqp_exchange "this is the RabbitMQ exchange name" $DEFAULT_AMQP_EXCHANGE $OLD_AMQP_EXCHANGE
     setEachNewValue $CONFIG_FILE log_file "this is to set the path of the log file" $DEFAULT_LOG_FILE $OLD_LOG_FILE
     setEachNewValue $CONFIG_FILE logging_level "this decides how much information to write to the log file, select one from 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'" $DEFAULT_LOGGING_LEVEL $OLD_LOGGING_LEVEL
-    
-	detected_cache=$(detectCacheType)
-    detected_port=$(getCachePort "$detected_cache")	
-	
-	setEachNewValue $CONFIG_FILE squid_port "this is the cache server port" $detected_port $OLD_CACHE_PORT	
-
-	if [ ! -z "$detected_cache" ]; then
-    	sed -i "s|^cache_type=.*|cache_type=$detected_cache|g" $CONFIG_FILE
-	fi
-	setEachNewValue $CONFIG_FILE cache_type "this is the cache server type (squid or varnish)" "$detected_cache" "$OLD_CACHE_TYPE"
-	
-    ENTERED_CACHE_TYPE=$(grep "^cache_type=" $CONFIG_FILE | cut -d'=' -f2) 	
-    if [ "$ENTERED_CACHE_TYPE" == "varnish" ]; then
-        while true; do
-        setEachNewValue $CONFIG_FILE upstream "this is the varnish server upstream (cvmfs or frontier)" $DEFAULT_UPSTREAM $OLD_UPSTREAM
-        ENTERED_UPSTREAM=$(grep "^upstream=" $CONFIG_FILE | cut -d'=' -f2)
-        if [ "$ENTERED_UPSTREAM" == "cvmfs" ] || [ "$ENTERED_UPSTREAM" == "frontier" ]; then
-            break
-        else
-            echo "Error: You must specify 'cvmfs' or 'frontier' for varnish upstream."
-            sed -i "s|^upstream=.*|upstream=|g" $CONFIG_FILE
-        fi
-    done
-    fi
-fi
-
+  
 # create log file and change ownership
 touch $LOG_FILE
 chown shoal:shoal $LOG_FILE
