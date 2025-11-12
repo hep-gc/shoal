ad74ba0fe4e6eb9929e9a46c59bdd136f8765c23
Seojin Lee <seojinlee0070@gmail.com>
2025-11-12 12:18:47 -0800

    Replace cvmfs_url with shoal_server_url in config

--- shoal-client/shoal_client/config.py
+++ shoal-client/shoal_client/config.py
@@ -78,10 +78,10 @@ except:
     raise
 
 # sets defaults to the options in config_file
-    
-if config_file.has_option("general", "cvmfs_url"):
-    cvmfs_url = config_file.get("general", "cvmfs_url")
 
+if config_file.has_option("general", "shoal_server_url"):
+    shoal_server_url = config_file.get("general", "shoal_server_url")
+    
 if config_file.has_option("general", "conditions_url"):
     conditions_url = config_file.get("general", "conditions_url")
     
